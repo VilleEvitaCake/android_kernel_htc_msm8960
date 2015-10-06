d4c3009d26c61b965d7c98d5a2a67c664119371f
Paul Keith <javelinanddart@gmail.com>
2015-10-05 19:57:01 -0500

    uberdemand: Don't Hardcode Second Phase Frequency
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- arch/arm/mach-msm/htc/ville/board-ville.c
+++ arch/arm/mach-msm/htc/ville/board-ville.c
@@ -3682,7 +3682,7 @@ static void __init ville_init(void)
        set_three_phase_freq_badass(CONFIG_CPU_FREQ_GOV_BADASS_3_PHASE_FREQ);
 #endif
 #ifdef CONFIG_CPU_FREQ_GOV_UBERDEMAND
-	set_second_phase_freq(1728000);
+	set_second_phase_freq(CONFIG_CPU_FREQ_GOV_UBERDEMAND_SECOND_PHASE_FREQ);
 #endif
 
 	ville_cable_detect_register();
