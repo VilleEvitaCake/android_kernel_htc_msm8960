dd9b4efcef9278a9cb5704b9bce453954abdf867
Paul Keith <javelinanddart@gmail.com>
2015-10-12 13:51:16 -0500

    ville & evita: Add Uberdemand 2nd Phase Hooks
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- arch/arm/mach-msm/htc/ville/board-ville.c
+++ arch/arm/mach-msm/htc/ville/board-ville.c
@@ -134,6 +134,9 @@ int set_two_phase_freq_badass(int cpufreq);
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_3_PHASE
 int set_three_phase_freq_badass(int cpufreq);
 #endif
+#ifdef CONFIG_CPU_FREQ_GOV_UBERDEMAND
+int set_second_phase_freq(int cpufreq);
+#endif
 
 extern unsigned int engineerid; // bit 0
 
@@ -3677,6 +3680,9 @@ static void __init ville_init(void)
 #endif
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_3_PHASE
        set_three_phase_freq_badass(CONFIG_CPU_FREQ_GOV_BADASS_3_PHASE_FREQ);
+#endif
+#ifdef CONFIG_CPU_FREQ_GOV_UBERDEMAND
+	set_second_phase_freq(1728000);
 #endif
 
 	ville_cable_detect_register();
